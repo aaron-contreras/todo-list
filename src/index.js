1904df6b084a59f89af24942f8f1fa4aef6a8f8d
Aaron Contreras <aaronlcaq@gmail.com>
2022-07-09 16:07:28 -0500

    [FEATURE] Expand acceptance test to multiple TodoItems
    In order to have a bigger base to test with, extended the dummy
    todoItems to a count of 3 and used `console.dir` to more quickly
    display the collection of TodoItem objects when visiting the
    browser console.

--- src/index.js
+++ src/index.js
@@ -10,11 +10,28 @@ function component() {
 
 document.body.appendChild(component());
 
-const itemOne = TodoItem({
-  title: 'Hello World',
-  description: 'First Todo Item',
-  dueDate: 'Tomorrow',
-  priority: 'High'
-});
+const todoItemsAttributes = [
+  {
+    title: 'Todo Item 1',
+    description: 'This is the first todo item',
+    dueDate: 'Tomorrow',
+    priority: 'High'
+  },
+  {
+    title: 'Todo Item 2',
+    description: 'This is the second todo item',
+    dueDate: 'Next Week',
+    priority: 'Medium'
+  },
+  {
+    title: 'Todo Item 3',
+    description: 'This is the third todo item',
+    dueDate: 'Next Month',
+    priority: 'Low'
+  },
+]
 
-console.log(itemOne);
+
+const todoItems = todoItemsAttributes.map( (attributes) => TodoItem(attributes));
+
+console.dir(todoItems);
