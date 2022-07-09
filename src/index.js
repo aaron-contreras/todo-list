252a0094fc4074654026f8dd3065d7b8a6ca625a
Aaron Contreras <aaronlcaq@gmail.com>
2022-07-09 16:22:49 -0500

    [REFACTOR] Extract dummy data into its own project section
    Following up on the previous refactor to organize the code
    structure, now "fixture" data lives in its own dummy-data
    directory. This furthers the code cleanliness and order.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import todoItemsAttributes from './dummy-data/todoItems';
 import TodoItem from './factories/todoItem';
 
 function component() {
@@ -9,28 +10,6 @@ function component() {
 }
 
 document.body.appendChild(component());
-
-const todoItemsAttributes = [
-  {
-    title: 'Todo Item 1',
-    description: 'This is the first todo item',
-    dueDate: 'Tomorrow',
-    priority: 'High'
-  },
-  {
-    title: 'Todo Item 2',
-    description: 'This is the second todo item',
-    dueDate: 'Next Week',
-    priority: 'Medium'
-  },
-  {
-    title: 'Todo Item 3',
-    description: 'This is the third todo item',
-    dueDate: 'Next Month',
-    priority: 'Low'
-  },
-]
-
 
 const todoItems = todoItemsAttributes.map( (attributes) => TodoItem(attributes));
 
