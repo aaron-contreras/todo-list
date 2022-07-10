e34fd077d96e9bf89daaeb8cc35110ec275dc86e
Aaron Contreras <aaronlcaq@gmail.com>
2022-07-10 12:56:35 -0500

    [FEATURE] Add project dummy data
    Sets up default "fixtures" to test with and assert that behavior
    is correct.

---
+++ src/dummy-data/projects.js
@@ -0,0 +1,13 @@
+import todoItemsAttributes from './todoItems';
+
+const projectsAttributes = [
+	{
+		name: 'Default',
+		todoItems: todoItemsAttributes.slice(0, -1)
+	},
+	{
+		name: 'Data Structures Final Project',
+	}
+];
+
+export default projectsAttributes;
